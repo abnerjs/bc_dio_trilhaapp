1cd83b9f249268c469708db858d5b0d620df8bb6
Abner Silva <abner.js05@gmail.com>
2023-08-23 15:54:53 -0300

    added textfields and button, make the main Container a CustomScrollView with a SilverFillRemaining

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,115 +15,138 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.only(top: 100, left: 40, right: 40),
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 40),
-              child: Text(
-                "TrilhaApp",
-                style: GoogleFonts.poppins(
-                  color: const Color.fromARGB(255, 0, 0, 0),
-                  fontWeight: FontWeight.w400,
-                  fontSize: 30,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 0),
-              child: Text(
-                "Bem vindo de volta!",
-                style: GoogleFonts.poppins(
-                  color: const Color.fromARGB(255, 0, 0, 0),
-                  fontWeight: FontWeight.w400,
-                  fontSize: 30,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 40),
-              child: Row(
-                children: <Widget>[
-                  Flexible(
+    return SafeArea(
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin:
+                        const EdgeInsets.only(bottom: 40, top: 100, left: 40),
                     child: Text(
-                      "Faça login para continuar.",
+                      "TrilhaApp",
                       style: GoogleFonts.poppins(
-                        color: const Color.fromARGB(255, 170, 170, 170),
+                        color: const Color.fromARGB(255, 0, 0, 0),
                         fontWeight: FontWeight.w400,
-                        fontSize: 24,
+                        fontSize: 30,
                       ),
                     ),
                   ),
-                ],
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              height: 60,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: const Color.fromARGB(255, 240, 240, 240),
-              ),
-              child: const Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 40),
                     child: Text(
-                      "Email",
-                      style: TextStyle(
-                        fontSize: 16,
+                      "Bem vindo de volta!",
+                      style: GoogleFonts.poppins(
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 30,
                       ),
                     ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              height: 60,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: const Color.fromARGB(255, 240, 240, 240),
-              ),
-              child: const Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Text(
-                      "Senha",
-                      style: TextStyle(
-                        fontSize: 16,
+                  ),
+                  Container(
+                    margin:
+                        const EdgeInsets.only(bottom: 40, left: 40, right: 40),
+                    child: Row(
+                      children: <Widget>[
+                        Flexible(
+                          child: Text(
+                            "Faça login para continuar.",
+                            style: GoogleFonts.poppins(
+                              color: const Color.fromARGB(255, 170, 170, 170),
+                              fontWeight: FontWeight.w400,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin:
+                        const EdgeInsets.only(bottom: 10, left: 40, right: 40),
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: const Color.fromARGB(255, 240, 240, 240),
+                    ),
+                    child: const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 240, 240, 240),
+                        labelText: 'Email',
+                        border: InputBorder.none,
                       ),
                     ),
-                  )
+                  ),
+                  Container(
+                    margin:
+                        const EdgeInsets.only(bottom: 10, left: 40, right: 40),
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: const Color.fromARGB(255, 240, 240, 240),
+                    ),
+                    child: const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 240, 240, 240),
+                        labelText: 'Senha',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 40),
+                    child: TextButton(
+                        onPressed: () => {},
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          "Entrar",
+                          style: GoogleFonts.poppins(
+                            color: Colors.deepPurple,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 24,
+                          ),
+                        )),
+                  ),
+                  Expanded(child: Container()),
+                  Container(
+                    margin:
+                        const EdgeInsets.only(bottom: 40, left: 40, right: 40),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Não tem uma conta?",
+                          style: GoogleFonts.poppins(
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(
+                          " Cadastre-se ",
+                          style: GoogleFonts.poppins(
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              child: Text(
-                "Entrar",
-                style: GoogleFonts.poppins(
-                  color: Colors.deepPurple,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            Expanded(child: Container()),
-            Container(
-              margin: const EdgeInsets.only(bottom: 40),
-              child: Text(
-                "Não tem uma conta? Cadastre-se",
-                style: GoogleFonts.poppins(
-                  color: const Color.fromARGB(255, 0, 0, 0),
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14,
-                ),
               ),
             ),
           ],
