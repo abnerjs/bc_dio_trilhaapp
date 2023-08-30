bc6fdec733b7a7191964e8802d1e63cb53d5a7c3
Abner Silva <abner.js05@gmail.com>
2023-08-29 23:03:33 -0300

    fix clock on login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -18,168 +18,169 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin:
-                        const EdgeInsets.only(bottom: 40, top: 100, left: 40),
-                    child: Text(
-                      widget.title,
-                      style: GoogleFonts.poppins(
-                        color: const Color.fromARGB(255, 0, 0, 0),
-                        fontWeight: FontWeight.w400,
-                        fontSize: 30,
-                      ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 40, top: 20, left: 40),
+                  child: Text(
+                    widget.title,
+                    style: GoogleFonts.poppins(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 30,
                     ),
                   ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 40),
-                    child: Text(
-                      "Bem vindo de volta!",
-                      style: GoogleFonts.poppins(
-                        color: const Color.fromARGB(255, 0, 0, 0),
-                        fontWeight: FontWeight.w400,
-                        fontSize: 30,
-                      ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Text(
+                    "Bem vindo de volta!",
+                    style: GoogleFonts.poppins(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 30,
                     ),
                   ),
-                  Container(
-                    margin:
-                        const EdgeInsets.only(bottom: 40, left: 40, right: 40),
-                    child: Row(
-                      children: <Widget>[
-                        Flexible(
-                          child: Text(
-                            "Faça login para continuar.",
-                            style: GoogleFonts.poppins(
-                              color: const Color.fromARGB(255, 170, 170, 170),
-                              fontWeight: FontWeight.w400,
-                              fontSize: 24,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin:
-                        const EdgeInsets.only(bottom: 10, left: 40, right: 40),
-                    height: 60,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: const Color.fromARGB(255, 240, 240, 240),
-                    ),
-                    child: TextField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                        filled: true,
-                        fillColor: Color.fromARGB(255, 240, 240, 240),
-                        labelText: 'Email',
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin:
-                        const EdgeInsets.only(bottom: 10, left: 40, right: 40),
-                    height: 60,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: const Color.fromARGB(255, 240, 240, 240),
-                    ),
-                    child: TextField(
-                      controller: passwordController,
-                      obscureText: !isPasswordVisible,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: const Color.fromARGB(255, 240, 240, 240),
-                        labelText: 'Senha',
-                        border: InputBorder.none,
-                        suffixIcon: InkWell(
-                          onTap: () {
-                            setState(() {
-                              isPasswordVisible = !isPasswordVisible;
-                            });
-                          },
-                          child: Icon(
-                            isPasswordVisible
-                                ? Icons.visibility
-                                : Icons.visibility_off,
-                            color: const Color.fromARGB(255, 170, 170, 170),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 40),
-                    child: TextButton(
-                        onPressed: () {
-                          if (emailController.text == "admin" &&
-                              passwordController.text == "admin") {
-                            Navigator.pushReplacementNamed(context, '/home');
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                backgroundColor: Colors.deepPurple,
-                                content: Text('Usuário ou senha inválidos'),
-                              ),
-                            );
-                          }
-                        },
-                        style: ButtonStyle(
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                        ),
+                ),
+                Container(
+                  margin:
+                      const EdgeInsets.only(bottom: 40, left: 40, right: 40),
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
                         child: Text(
-                          "Entrar",
+                          "Faça login para continuar.",
                           style: GoogleFonts.poppins(
-                            color: Colors.deepPurple,
+                            color: const Color.fromARGB(255, 170, 170, 170),
                             fontWeight: FontWeight.w400,
                             fontSize: 24,
                           ),
-                        )),
+                        ),
+                      ),
+                    ],
                   ),
-                  Expanded(child: Container()),
-                  Container(
-                    margin:
-                        const EdgeInsets.only(bottom: 40, left: 40, right: 40),
-                    child: Row(
-                      children: [
-                        Text(
-                          "Não tem uma conta?",
-                          style: GoogleFonts.poppins(
-                            color: const Color.fromARGB(255, 0, 0, 0),
-                            fontWeight: FontWeight.w400,
-                            fontSize: 14,
-                          ),
-                        ),
-                        Text(
-                          " Cadastre-se ",
-                          style: GoogleFonts.poppins(
-                            color: const Color.fromARGB(255, 0, 0, 0),
-                            fontWeight: FontWeight.w600,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
+                ),
+                Container(
+                  margin:
+                      const EdgeInsets.only(bottom: 10, left: 40, right: 40),
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: const Color.fromARGB(255, 240, 240, 240),
+                  ),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 240, 240, 240),
+                      labelText: 'Email',
+                      border: InputBorder.none,
                     ),
                   ),
-                ],
-              ),
+                ),
+                Container(
+                  margin:
+                      const EdgeInsets.only(bottom: 10, left: 40, right: 40),
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: const Color.fromARGB(255, 240, 240, 240),
+                  ),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: !isPasswordVisible,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color.fromARGB(255, 240, 240, 240),
+                      labelText: 'Senha',
+                      border: InputBorder.none,
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          });
+                        },
+                        child: Icon(
+                          isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: const Color.fromARGB(255, 170, 170, 170),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 40),
+                  child: TextButton(
+                      onPressed: () {
+                        if (emailController.text == "admin" &&
+                            passwordController.text == "admin") {
+                          Navigator.pushReplacementNamed(context, '/home');
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              backgroundColor: Colors.deepPurple,
+                              content: Text('Usuário ou senha inválidos'),
+                            ),
+                          );
+                        }
+                      },
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                      child: Text(
+                        "Entrar",
+                        style: GoogleFonts.poppins(
+                          color: Colors.deepPurple,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 24,
+                        ),
+                      )),
+                ),
+                Expanded(child: Container()),
+                Container(
+                  margin:
+                      const EdgeInsets.only(bottom: 40, left: 40, right: 40),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Não tem uma conta?",
+                        style: GoogleFonts.poppins(
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        " Cadastre-se ",
+                        style: GoogleFonts.poppins(
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
